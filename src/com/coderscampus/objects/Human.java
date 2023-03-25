fd74d5de776b792387a67f553c0a13cd12cb795b
Knowlton2023 <122704053+Knowlton2023@users.noreply.github.com>
2023-03-25 16:24:19 -0500

    Initial commit

---
+++ src/com/coderscampus/objects/Human.java
@@ -0,0 +1,15 @@
+package com.coderscampus.objects;
+
+public class Human {
+
+	String gender;
+	String height;
+	String weight;
+	String eyeColor;
+	
+	// (visibility modifier), return type, method name, params, scope
+	String getInfo () {
+		return "Gender: " + gender + ", Height: " + height + ", Weight: " + weight +
+				", EyeColor: " + eyeColor;
+	}
+}
---
+++ src/com/coderscampus/objects/RunnableClass.java
@@ -0,0 +1,43 @@
+package com.coderscampus.objects;
+
+public class RunnableClass {
+
+	public static void main(String[] args) {
+		// Class -> Blueprint for an Object
+		// Object -> a Noun, it's an instantiation of a Class
+		
+		// Data type, variable name, assignment operation
+		int someNumber = 39;
+		
+		Human trevorPage = new Human();
+		
+		trevorPage.eyeColor = "brown";
+		trevorPage.gender = "male";
+		trevorPage.height = "5'10\"";
+		trevorPage.weight = "160";
+		
+		System.out.println(trevorPage.getInfo());
+		
+		
+		Human elonMusk = new Human();
+		
+		elonMusk.eyeColor = "blue";
+		elonMusk.gender = "male";
+		elonMusk.height = "6'1\"";
+		elonMusk.weight = "190";
+		
+		System.out.println(elonMusk.getInfo());
+	
+
+		Human christinaPage = new Human();
+		
+		christinaPage.eyeColor = "brown";
+		christinaPage.gender = "felmale";
+		christinaPage.height = "5'5\"";
+		christinaPage.weight = "130";
+		
+		System.out.println(christinaPage.getInfo());
+	
+	}
+
+}
